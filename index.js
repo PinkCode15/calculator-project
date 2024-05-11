a10d11a01fe30e705597296ec82bece4779a590a
StaceyCreates <stacey.onuora@mypatricia.co>
2024-05-11 03:19:55 +0100

    complete calculator

---
+++ index.js
@@ -0,0 +1,18 @@
+function appendToResult(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    let resultElement = document.getElementById("result");
+
+    try{
+        let result = eval(resultElement.value);
+        result = parseFloat(result.toFixed(10));
+        resultElement.value = result;
+    }catch(error){
+        console.log(error);
+        resultElement.value = 'Error';
+    }
+
+    
+}
